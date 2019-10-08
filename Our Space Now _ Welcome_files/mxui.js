3209a6e4e2a0d8959e065c06a62f609956d6de7f
Esseson <35272146+Esseson@users.noreply.github.com>
2019-10-08 08:58:05 +0300

    Webpage

---
+++ Our Space Now _ Welcome_files/mxui.js
@@ -0,0 +1,36 @@
+function addx(a){
+displaylightbox('https://ourspacenow.site/'+ a,{width:700,height:630,'transitionIn':'elastic','transitionOut':'elastic','speedIn':600,'speedOut':200,scrolling:'yes',overlayShow:true,overlayColor:'#FFFFFF'});
+}
+
+
+function add(){
+displaylightbox('https://ourspacenow.site/g:addxr',{width:500,height:600,'transitionIn':'elastic','transitionOut':'elastic','speedIn':600,'speedOut':200,scrolling:'yes',overlayShow:true,overlayColor:'#FFFFFF'});
+}
+
+
+function cpx(a) {
+    displaylightbox('https://ourspacenow.site/'+ a ,{width:500,height:600,'transitionIn':'elastic','transitionOut':'elastic','speedIn':600,'speedOut':200,scrolling:'yes',overlayShow:true,overlayColor:'#FFFFFF'});
+    }
+
+    function ddx(a) {
+        displaylightbox('https://ourspacenow.site/'+ a ,{width:500,height:600,'transitionIn':'elastic','transitionOut':'elastic','speedIn':600,'speedOut':200,scrolling:'yes',overlayShow:true,overlayColor:'#FFFFFF'});
+        }
+    
+        function settings(){
+            displaylightbox('https://ourspacenow.site/g:change_password_form',{width:500,height:600,'transitionIn':'elastic','transitionOut':'elastic','speedIn':600,'speedOut':200,scrolling:'yes',overlayShow:true,overlayColor:'#FFFFFF'});
+            }
+
+
+            function mcenter(){
+                displaylightbox('https://ourspacenow.site/g:mail_list',{width:600,height:450,'transitionIn':'elastic','transitionOut':'elastic','speedIn':600,'speedOut':200,scrolling:'yes',overlayShow:true,overlayColor:'#FFFFFF'});
+                }     
+
+            function updx(){
+                window.location.reload();
+                }
+            
+
+                function xs(a){
+                    window.location.href= a;
+                    }
+                    
